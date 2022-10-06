8192856e3326d74f97e11908eff1105e27cf5e48
Quezia Aquino <q.de.oliveira.aquino@avanade.com>
2022-10-06 19:45:51 -0300

    first commit

---
+++ src/images.js
@@ -0,0 +1,8 @@
+import i1 from './assets/1.jpg';
+import i2 from './assets/2.jpg';
+import i3 from './assets/3.jpg';
+import i4 from './assets/4.jpg';
+import i5 from './assets/5.jpg';
+import i6 from './assets/6.jpg';
+
+export default [i1, i2, i3, i4, i5, i6];
